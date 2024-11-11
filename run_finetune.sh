86c18c746305985378015f3e0cb0e2cb3c8bf7dd
tim-hudelmaier <tim.hudelmaier@stud.uni-heidelberg.de>
2024-11-11 14:20:12 +0100

    simplify script and add logging

--- run_finetune.sh
+++ run_finetune.sh
@@ -7,10 +7,12 @@
 #SBATCH -p gpu-el8 -C gaming -n 16 --gres=gpu:3090:1 --mem-per-gpu 64283
 #SBATCH --mail-type=FAIL,BEGIN,END
 #SBATCH --mail-user=tim.hudelmaier@embl.de
+#SBATCH -o /scratch/thudelmaier/micro-sam/logs/slurm.%N.%j.out
+#SBATCH -e /scratch/thudelmaier/micro-sam/logs/slurm.%N.%j.err
 
 module load CUDA/12.5.0
 singularity exec --nv --bind /scratch/thudelmaier/micro-sam:/scratch/thudelmaier/micro-sam micro-sam.sif \
-  bash -c "mamba init && exec bash && mamba activate micro-sam && \
+  bash -c "source activate micro-sam && \
   python /scratch/thudelmaier/micro-sam/finetune.py \
   --config_path /scratch/thudelmaier/micro-sam/default_config.json \
   --img_dir /scratch/thudelmaier/micro-sam/avia \
